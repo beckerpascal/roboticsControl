0a51d16425f4a9bc7c5e939f81011cb551ea3b6d
Pascal Becker <pascal@beckerpascal.de>
2015-02-11 11:30:25 +0200

    remote connection added

--- simulation.py
+++ simulation.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 
-print("Hello from Python world!)
+import vrep
+import sys
 
+print 'Program started'
+vrep.simxFinish(-1) # just in case, close all opened connections
+clientID=vrep.simxStart('127.0.0.1',int (sys.argv[1]),True,True,5000,5)
+if clientID!=-1:
+    print 'Connected to remote API server'
+    res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_oneshot_wait)
+    if res==vrep.simx_return_ok:
+        print 'Number of objects in the scene: ',len(objs)
+    else:
+        print 'Remote API function call returned with error code: ',res
+    vrep.simxFinish(clientID)
+else:
+    print 'Failed connecting to remote API server'
+print 'Program ended'
