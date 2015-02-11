17e332f733fc67204d626e920ece8d14aab62905
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-02-11 13:55:45 +0200

    Does work now. Changed port to 19999.

--- simulation.py
+++ simulation.py
@@ -4,17 +4,25 @@ import sys
 import vrep
 
 print 'Program started'
-port = int(sys.argv[1])
 vrep.simxFinish(-1) # just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1',port,True,True,5000,5)
+
+addr = '127.0.0.1'
+port = int(sys.argv[1] if len(sys.argv) > 1 else 19999)
+print 'Connecting to %s:%d' % (addr, port)
+
+clientID=vrep.simxStart(addr, port, True, True, 5000, 5)
 if clientID!=-1:
-    print 'Connected to remote API server'
+    print 'Connected to remote API server with id: %d' % (clientID,)
+    errorCode = vrep.simxAddStatusbarMessage(clientID, 'Python says hi.', vrep.simx_opmode_oneshot_wait)
+    print 'AddStatusBarMessage status: %d' % errorCode
     res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_oneshot_wait)
     if res==vrep.simx_return_ok:
         print 'Number of objects in the scene: ',len(objs)
     else:
         print 'Remote API function call returned with error code: ',res
-    vrep.simxFinish(clientID)
+    print 'asd'
+    vrep.simxFinish(-1)
+    print 'asd'
 else:
     print 'Failed connecting to remote API server'
 print 'Program ended'
