4e2084ab83b35df3ba99e8255afde8cd4b186109
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-02-11 14:02:43 +0200

    Launch the script from v-rep once again.

--- simulation.py
+++ simulation.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
-
 import sys
 import vrep
 
-print 'Program started'
+print '-- Starting Python client'
 vrep.simxFinish(-1) # just in case, close all opened connections
 
 addr = '127.0.0.1'
 port = int(sys.argv[1] if len(sys.argv) > 1 else 19999)
-print 'Connecting to %s:%d' % (addr, port)
+print '-- Connecting to %s:%d' % (addr, port)
 
-clientID=vrep.simxStart(addr, port, True, True, 5000, 5)
-if clientID!=-1:
-    print 'Connected to remote API server with id: %d' % (clientID,)
-    errorCode = vrep.simxAddStatusbarMessage(clientID, 'Python says hi.', vrep.simx_opmode_oneshot_wait)
-    print 'AddStatusBarMessage status: %d' % errorCode
-    res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_oneshot_wait)
-    if res==vrep.simx_return_ok:
-        print 'Number of objects in the scene: ',len(objs)
+client = vrep.simxStart(addr, port, True, True, 5000, 5)
+
+if client != -1:
+    print '-- Connected to remote API server with id: %d' % (client)
+    errorCode = vrep.simxAddStatusbarMessage(client, '-- Client connected at port %d' % port, vrep.simx_opmode_oneshot_wait)
+
+    res, objs = vrep.simxGetObjects(client, vrep.sim_handle_all, vrep.simx_opmode_oneshot_wait)
+    if res == vrep.simx_return_ok:
+        print 'Number of objects in the scene: ', len(objs)
     else:
-        print 'Remote API function call returned with error code: ',res
-    print 'asd'
+        print 'Remote API function call returned with error code: ', res
     vrep.simxFinish(-1)
-    print 'asd'
 else:
-    print 'Failed connecting to remote API server'
-print 'Program ended'
+    print '-- Failed connecting to remote API server'
+print '-- Terminating Python client'
