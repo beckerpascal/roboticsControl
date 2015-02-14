a432476cdc1f8af5761e889818e15633e5583c70
Pascal Becker <pascal@beckerpascal.de>
2015-02-14 18:33:21 +0200

    Fixed some bugs from merging the branches

--- simulation.py
+++ simulation.py
@@ -31,11 +31,14 @@ if __name__ == '__main__':
         else:
             print 'Remote API function call returned with error code: ', res
 
+        # init left motor
         res, left_motor = simxGetObjectHandle(client, 'leftMotor', simx_opmode_oneshot_wait)
         if res == simx_return_ok:
             log(client, 'leftMotor handle: %d' % left_motor)
         else:
             print 'Remote API function call returned with error code: ', res
+
+        # init right motor
         res, right_motor = simxGetObjectHandle(client, 'rightMotor', simx_opmode_oneshot_wait)
         if res == simx_return_ok:
             log(client, 'rightMotor handle: %d' % right_motor)
@@ -45,13 +48,11 @@ if __name__ == '__main__':
         for i in xrange(20):
             errLeft = simxSetJointForce(client, left_motor, 100000, simx_opmode_oneshot_wait)
             errRight = simxSetJointForce(client, right_motor, -100000, simx_opmode_oneshot_wait)
-    res,objs=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_oneshot_wait)
-    if res==vrep.simx_return_ok:
             log(client, str(errLeft) + ' ' + str(errRight))
         simxFinish(-1)
     else:
         print '-- Failed connecting to remote API server'
-    vrep.simxFinish(clientID)
+    simxFinish(-1)
 else:
     print '-- Terminating Python client'
-print 'Program ended'
+print 'Program ended without errors'
