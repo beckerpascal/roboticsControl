1cdfd7a5deb3224f602fe68a1dc59bbf2a7f8f9e
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-02-19 18:59:39 +0200

    Repurpose the simulation.py as a master (coordinator) client.

--- simulation.py
+++ simulation.py
@@ -1,65 +1,34 @@
 #!/usr/bin/env python
 import sys
 from vrep import *
-
-def log(client, message):
-    err = simxAddStatusbarMessage(client, '-- ' + message, simx_opmode_oneshot_wait)
-    if err:
-        print 'AddStatusBarMessage errors with: %d' % err
+from util import *
 
 
 ##############################################################################
 # MAIN
 ##############################################################################
 if __name__ == '__main__':
-    print '-- Starting Python client'
+    print '-- Starting master client'
     simxFinish(-1) # just in case, close all opened connections
 
+    # localhost:19997 connects to V-REP global remote API
     addr = '127.0.0.1'
-    port = int(sys.argv[1] if len(sys.argv) > 1 else 19999)
+    port = 19997
     print '-- Connecting to %s:%d' % (addr, port)
-
     client = simxStart(addr, port, True, True, 5000, 5)
 
     if client != -1:
-        print '-- Connected to remote API server with id: %d' % (client)
-        log(client, 'Client connected at port %d' % port)
+        log(client, 'Master client connected to client %d at port %d' % (client, port))
 
-        res, objs = simxGetObjects(client, sim_handle_all, simx_opmode_oneshot_wait)
-        if res == simx_return_ok:
-            print 'Number of objects in the scene: ', len(objs)
+        err, objs = simxGetObjects(client, sim_handle_all, simx_opmode_oneshot_wait)
+        if err == simx_return_ok:
+            log(client, 'Number of objects in the scene: %d' % len(objs))
         else:
-            print 'Remote API function call returned with error code: ', res
+            log(client, 'ERROR GetObjects code %d' % res)
 
-        # init left motor
-        res, left_motor = simxGetObjectHandle(client, 'leftMotor', simx_opmode_oneshot_wait)
-        if res == simx_return_ok:
-            log(client, 'leftMotor handle: %d' % left_motor)
-        else:
-            print 'Remote API function call returned with error code: ', res
-
-        # init right motor
-        res, right_motor = simxGetObjectHandle(client, 'rightMotor', simx_opmode_oneshot_wait)
-        if res == simx_return_ok:
-            log(client, 'rightMotor handle: %d' % right_motor)
-        else:
-            print 'Remote API function call returned with error code: ', res
-
-        sign = 1
-        #for i in xrange(20):
-        errLeft = simxSetJointForce(client, left_motor, 100000, simx_opmode_oneshot_wait)
-        errRight = simxSetJointForce(client, right_motor, -100000, simx_opmode_oneshot_wait)
-        log(client, 'Error left/right: ' + str(errLeft) + ' ' + str(errRight))
-
-        #sign *= -1
-        valLeft = simxSetJointTargetVelocity(client,left_motor,1,simx_opmode_oneshot)
-        valRight = simxSetJointTargetVelocity(client,right_motor,1,simx_opmode_oneshot)
-        log(client, 'Values left/right: ' + str(valLeft) + ' ' + str(valRight))
-            
         simxFinish(-1)
     else:
         print '-- Failed connecting to remote API server'
+
     simxFinish(-1)
-else:
     print '-- Terminating Python client'
-print 'Program ended without errors'
