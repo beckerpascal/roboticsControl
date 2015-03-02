5b129235c4c4c42e54c4dd109488a1db04f038a9
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-03-02 20:50:38 +0200

    Make the final value reporting more reliable...
    ...for reasons I cannot understand for now. :/

--- twiddle.py
+++ twiddle.py
@@ -6,7 +6,7 @@ class Twiddle:
     Twiddler class
     """
 
-    def __init__(self, params=[0, 0, 0], deltas=[1, 1, 1], tolerance=10**-6):
+    def __init__(self, params=[0, 0, 0], deltas=[2, 1, 1], tolerance=10**-6):
         self.params = params
         self.deltas = deltas
         self.tolerance = tolerance
@@ -18,7 +18,7 @@ class Twiddle:
         """
         # Initialize a buffered vector for the best parameters found for
         # cleaner keyboard interrupt handling
-        best_params = [-1 for p in self.params]
+        self.best_params = [-1 for p in self.params]
         # Catch ctrl-Cs here since the interesting data (best params) are here
         try:
             # Coordinate descent -> tolerance will be on the param deltas
@@ -30,6 +30,7 @@ class Twiddle:
                     if error < self.error_min:
                         # New best result
                         self.error_min = error
+                        self.best_params = self.params
                         self.deltas[i] *= 1.1
                     else:
                         # Was not better, try other way around
@@ -38,13 +39,13 @@ class Twiddle:
                         if error < self.error_min:
                             # Was best when going the other way
                             self.error_min = error
+                            self.best_params = self.params
                             self.deltas[i] *= 1.1
                         else:
                             # Didn't get better results using previous delta.
                             # Thus, make it smaller and try again
                             self.params[i] += self.deltas[i]
                             self.deltas[i] *= 0.9
-                best_params = self.params
         except KeyboardInterrupt:
-            pass  # Do nothing and then pass the params
-        return best_params
+            print "ctrl-C!"  # Do nothing and then pass the params
+        return self.best_params
